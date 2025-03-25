ed462cf761f72b82c7a3c3e66eec0071842bc7db
Anton Ovod <antovod36@gmail.com>
2025-03-25 20:30:13 +0100

    [FRONTEND] Create store slice for auth using zustand.

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+interface AuthUser {
+  _id: string;
+  fullName: string;
+  email: string;
+  profilePic: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface AuthStore {
+  authUser: AuthUser | null;
+  isSigningUp: boolean;
+  isLoggingIn: boolean;
+  isUpdatingProfile: boolean;
+  isCheckingAuth: boolean;
+  checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get<AuthUser>("/auth/check");
+
+      set({ authUser: response.data });
+    } catch (error) {
+      console.log("Error in checkAuth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
