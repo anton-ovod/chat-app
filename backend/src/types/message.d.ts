8011d3a2a98b4325d4169c550b6e73a33509d466
Anton Ovod <antovod36@gmail.com>
2025-04-11 11:49:27 +0200

    [BACKEND] Modified message model. Added conversationId field.

--- backend/src/types/message.d.ts
+++ backend/src/types/message.d.ts
@@ -3,6 +3,7 @@ import { Types } from "mongoose";
 export interface IMessage {
   senderId: Types.ObjectId;
   receiverId: Types.ObjectId;
+  conversationId: Types.ObjectId;
   text?: string;
   image?: string;
   createdAt: Date;
