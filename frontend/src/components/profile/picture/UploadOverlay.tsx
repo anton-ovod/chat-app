cc60e6c28dd8b529b44de07146aa15685248bce9
Anton Ovod <antovod36@gmail.com>
2025-04-22 19:15:12 +0200

    refactor(picture-uploader): encapsulate logic inside corresponding components, cleanup

--- frontend/src/components/profile/picture/UploadOverlay.tsx
+++ frontend/src/components/profile/picture/UploadOverlay.tsx
@@ -1,30 +1,45 @@
 import { Camera } from "lucide-react";
 import { FC } from "react";
+import { useProfileStore } from "../../../store/useProfileStore";
 
-type UploadOverlayProps = {
-  isDisabled: boolean;
-  onUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
-};
+const UploadOverlay: FC = () => {
+  const { isUpdatingProfile, setProfileData } = useProfileStore();
 
-const UploadOverlay: FC<UploadOverlayProps> = ({ isDisabled, onUpload }) => (
-  <label
-    htmlFor="avatar-upload"
-    className={`
+  const handleImageUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64Image = reader.result as string;
+      setProfileData((prev) => ({ ...prev, profilePic: base64Image }));
+    };
+  };
+
+  return (
+    <label
+      htmlFor="avatar-upload"
+      className={`
       absolute bottom-0 right-0 bg-base-content hover:scale-105 p-2 
       rounded-full cursor-pointer transition-all duration-200
-      ${isDisabled ? "cursor-not-allowed" : ""}
+      ${isUpdatingProfile ? "cursor-not-allowed" : ""}
     `}
-  >
-    <Camera className="w-5 h-5 text-base-200" />
-    <input
-      type="file"
-      id="avatar-upload"
-      className="hidden"
-      accept="image/*"
-      onChange={onUpload}
-      disabled={isDisabled}
-    />
-  </label>
-);
+    >
+      <Camera className="w-5 h-5 text-base-200" />
+      <input
+        type="file"
+        id="avatar-upload"
+        className="hidden"
+        accept="image/*"
+        onChange={handleImageUpload}
+        disabled={isUpdatingProfile}
+      />
+    </label>
+  );
+};
 
 export default UploadOverlay;
