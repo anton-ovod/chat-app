91e61db8d121af8c9996bcd77da7a2f93aeac78f
Anton Ovod <antovod36@gmail.com>
2025-04-22 19:02:02 +0200

    feat(frontend): allow to customize submit buttom using className prop.

--- frontend/src/components/buttons/SubmitButton.tsx
+++ frontend/src/components/buttons/SubmitButton.tsx
@@ -4,11 +4,20 @@ import { FC } from "react";
 type SubmitButtonProps = {
   isProcessing: boolean;
   label: string;
+  className?: string;
 };
 
-const SubmitButton: FC<SubmitButtonProps> = ({ isProcessing, label }) => {
+const SubmitButton: FC<SubmitButtonProps> = ({
+  isProcessing,
+  label,
+  className = "",
+}) => {
   return (
-    <button type="submit" className="btn btn-primary" disabled={isProcessing}>
+    <button
+      type="submit"
+      className={`btn btn-primary ${className}`}
+      disabled={isProcessing}
+    >
       {isProcessing ? (
         <>
           <Loader2 className="h-5 w-5 animate-spin" />
